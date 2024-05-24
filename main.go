5dac1b9e42a11fb07accc41490f6816ff8b3b8ae
supiz7z <burubulka12@gmail.com>
2024-05-24 20:10:08 +0300

    main 1

--- main.go
+++ main.go
@@ -4,20 +4,4 @@ import "fmt"
 
 func main() {
 	fmt.Println("main 1")
-	fmt.Println("local 1")
-	tryRebase()
-	tryRebaseToMain()
-	victory()
-}
-
-func tryRebase() {
-	fmt.Println("rebased")
-}
-
-func tryRebaseToMain() {
-	fmt.Println("to main")
-}
-
-func victory() {
-	fmt.Println("pidorasina")
 }
