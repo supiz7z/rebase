8501fca57e61f669606da00665cf726d3c1305a0
supiz7z <burubulka12@gmail.com>
2024-05-24 19:43:40 +0300

    main 1

--- main.go
+++ main.go
@@ -3,16 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("start at main")
-	fmt.Println("start at local")
-	testRebase()
-	fmt.Println("local 3")
-	fmt.Println("main 3")
-	fmt.Println("local 4")
-	fmt.Println("main 4")
-	fmt.Println("main 5")
-}
-
-func testRebase() {
-	fmt.Println("rebased")
+	fmt.Println("main 1")
 }
