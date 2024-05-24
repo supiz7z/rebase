7fa11e42b2ac4d7bbff2e3e7df328ef70bb71f53
supiz7z <burubulka12@gmail.com>
2024-05-24 17:26:14 +0300

    testing rebase

--- main.go
+++ main.go
@@ -4,9 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("start at main")
+	localBranchOne()
 	mainFunc()
 }
 
 func mainFunc() {
 	fmt.Println("created at main")
+}
+
+func localBranchOne() {
+	fmt.Println("created new branch")
 }
