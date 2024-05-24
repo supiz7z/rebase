ab20e10039571784d0f97d423a38c13618d8f804
supiz7z <burubulka12@gmail.com>
2024-05-24 17:59:04 +0300

    empty main

--- main.go
+++ main.go
@@ -4,14 +4,4 @@ import "fmt"
 
 func main() {
 	fmt.Println("start at main")
-	localBranchOne()
-	mainFunc()
-}
-
-func mainFunc() {
-	fmt.Println("created at main")
-}
-
-func localBranchOne() {
-	fmt.Println("created new branch")
 }
