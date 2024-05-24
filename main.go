125c169e4335936e96f3aa5a1f7404bff699b605
supiz7z <burubulka12@gmail.com>
2024-05-24 19:53:53 +0300

    local 2

--- main.go
+++ main.go
@@ -6,8 +6,13 @@ func main() {
 	fmt.Println("main 1")
 	fmt.Println("local 1")
 	tryRebase()
+	tryRebaseToMain()
 }
 
 func tryRebase() {
 	fmt.Println("rebased")
+}
+
+func tryRebaseToMain() {
+	fmt.Println("to main")
 }
